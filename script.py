c5e4d541e3ed218d0e6a37508a6957a2ce5b7465
Garrettvk <50793514+Garrettvk@users.noreply.github.com>
2020-06-01 02:24:00 -0700

    input_field_name added and input.csv changed

--- script.py
+++ script.py
@@ -4,11 +4,15 @@ This file prints each cell of dataframe individually
 
 import pandas as pd
 
-input_csv_df = pd.read_csv('input.csv')
+input_csv_df = pd.read_csv('input.csv', header=None)
+
+# productname,category1,category2,category3,image1,image2,description,price
+input_fields = input_csv_df.loc[0]
 
 number_of_rows = len(input_csv_df)
 
-for row in range(number_of_rows):     # for each row in dataframe
-  current_row = input_csv_df.loc[row] # variable for current row of iteration
-  for cell in current_row:            # print every cell in row
+for row in range(number_of_rows):             # for each row in dataframe
+  current_row = input_csv_df.loc[row]         # variable for current row of iteration
+  for index, cell in enumerate(current_row):  # iterate though index and cells
+    input_field_name = input_fields[index]    # current: productname,category1,category2,category3,image1,image2,description,price
     print(cell)
