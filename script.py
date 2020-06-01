3160a5e47fd6199a8669d1f46a4da7887e326c30
Garrettvk <50793514+Garrettvk@users.noreply.github.com>
2020-06-01 05:55:06 -0700

    input and output csvs are the same

--- script.py
+++ script.py
@@ -15,4 +15,8 @@ for row in range(number_of_rows):             # for each row in dataframe
   current_row = input_csv_df.loc[row]         # variable for current row of iteration
   for index, cell in enumerate(current_row):  # iterate though index and cells
     input_field_name = input_fields[index]    # current: productname,category1,category2,category3,image1,image2,description,price
-    print(cell)
+
+    if cell == input_field_name: # if cell is a field name it needs to be skipped
+      pass
+    else:
+      print(cell)
