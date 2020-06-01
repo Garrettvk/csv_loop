88922d08ecbf7d34fffbdebf322a86232d395683
Garrettvk <50793514+Garrettvk@users.noreply.github.com>
2020-06-01 05:55:26 -0700

    leftovers from last commit

--- script.py
+++ script.py
@@ -6,8 +6,13 @@ import pandas as pd
 
 input_csv_df = pd.read_csv('input.csv', header=None)
 
-# productname,category1,category2,category3,image1,image2,description,price
 input_fields = input_csv_df.loc[0]
+
+output_list = [] # each row is created in this list then appended to output dataframe
+
+output_df = pd.DataFrame(columns=input_fields)
+
+# productname,category1,category2,category3,image1,image2,description,price
 
 number_of_rows = len(input_csv_df)
 
@@ -19,4 +24,15 @@ for row in range(number_of_rows):             # for each row in dataframe
     if cell == input_field_name: # if cell is a field name it needs to be skipped
       pass
     else:
-      print(cell)
+
+
+      if index == 7:
+        output_list.append(cell) # add cell to list
+
+        output_df.loc[len(output_df)] = output_list # add list to data frame
+        output_list = [] # reset list
+      else: 
+
+        output_list.append(cell) # add cell to list
+
+output_df.to_csv('output.csv', index=False)
