14cb37e562ae550a080b4ca2b85bd2d286d69f99
Garrettvk <50793514+Garrettvk@users.noreply.github.com>
2020-06-01 01:57:13 -0700

    comments added

--- script.py
+++ script.py
@@ -4,11 +4,11 @@ This file prints each cell of dataframe individually
 
 import pandas as pd
 
-input_csv_df = pd.read_csv('input.csv') # df = the csv file we want to read
+input_csv_df = pd.read_csv('input.csv')
 
 number_of_rows = len(input_csv_df)
 
-for row in range(number_of_rows):
-  current_row = input_csv_df.loc[row]
-  for row in current_row:
-    print(row)
+for row in range(number_of_rows):     # for each row in dataframe
+  current_row = input_csv_df.loc[row] # variable for current row of iteration
+  for cell in current_row:            # print every cell in row
+    print(cell)
